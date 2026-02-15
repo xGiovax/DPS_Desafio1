c8c2558253e371587c8e931f42a916ea53a1aef7
Fernando Cornejo <cornejofernando212@gmail.com>
2026-02-15 12:11:51 -0600

    Actualizacion del componente Contact

--- src/componentes/Contact.jsx
+++ src/componentes/Contact.jsx
@@ -2,7 +2,7 @@ export default function Contact({ contact, onDelete, onToggleFavorite }) {
   return (
     <div className={`contact ${contact.favorito ? "favorite" : ""}`}>
       <h4>{contact.nombre} {contact.apellido}</h4>
-      <p>📞 {contact.telefono}</p>
+      <p> {contact.telefono}</p>
 
       <button onClick={() => onToggleFavorite(contact.id)}>
         {contact.favorito ? "Quitar favorito" : "Agregar a favoritos"}
